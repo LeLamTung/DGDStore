a050328bfaa079e18805005ae66bfd976bbabf41
LeLamTung <lelamtung1704@gmail.com>
2026-01-16 15:24:53 +0700

    Fix

--- src/middlewares/checkAuth.ts
+++ src/middlewares/checkAuth.ts
@@ -1,14 +1,14 @@
-import { Request, Response, NextFunction } from "express";
+// import { Request, Response, NextFunction } from "express";
 
-export const isAuthenticated = (req: Request, res: Response, next: NextFunction): void => {
-    if (!req.session.userIdLogin) {
-        // Nếu người dùng chưa đăng nhập, trả về phản hồi lỗi
-        res.status(401).json({
-            message: "Bạn chưa đăng nhập, vui lòng đăng nhập để tiếp tục!",
-            redirect: "http://localhost:3000/auth/signin"
-        });
-    } else {
-        // Nếu người dùng đã đăng nhập, tiếp tục xử lý yêu cầu
-        next();
-    }
-};
+// export const isAuthenticated = (req: Request, res: Response, next: NextFunction): void => {
+//     if (!req.session.userIdLogin) {
+//         // Nếu người dùng chưa đăng nhập, trả về phản hồi lỗi
+//         res.status(401).json({
+//             message: "Bạn chưa đăng nhập, vui lòng đăng nhập để tiếp tục!",
+//             redirect: "http://localhost:3000/auth/signin"
+//         });
+//     } else {
+//         // Nếu người dùng đã đăng nhập, tiếp tục xử lý yêu cầu
+//         next();
+//     }
+// };
